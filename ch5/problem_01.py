770bfdc7de9bb73854b0f6998fd5b5db550055f6
Qurayshkhan <96570166+Qurayshkhan@users.noreply.github.com>
2025-05-29 00:18:01 +0500

    ch5: problem solving

---
+++ ch5/problem_01.py
@@ -0,0 +1,9 @@
+words = {
+    "madad": "HELP",
+    "kursi": "Chair",
+    "billi": "Cat",
+}
+
+word = input("Enter a word: ");
+
+print(words[word]);
---
+++ ch5/problem_07.py
@@ -0,0 +1,5 @@
+s = {8,7,12,"Hassan", [1,2]}
+
+s[4][0] = 0;
+
+# we cannot change the value of set by using index because unhashable type: 'list'
